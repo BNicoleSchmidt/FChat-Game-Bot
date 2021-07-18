f6c324f4d3caee63c90758eb8436b048ae11ae94
BNicoleSchmidt <the.ringwyrm@gmail.com>
2021-07-18 02:21:59 -0500

    Basic join/leave and bottle spin commands added

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const connectionInfo = require("./connection_info");
 
 var fchat = new Fchat(config);
 
+const playerTracker = {}
+
+function wrapInUserTags(character) {
+    return `[user]${character}[/user]`
+}
+
+function currentPlayersList(channel) {
+    const players = playerTracker[channel]
+    return players.length ? `Current players are:\n${players.map(wrapInUserTags).join('\n')}` : 'No one is currently playing.'
+}
+
+function sendMSG(channel, message) {
+    fchat.send('MSG', { channel, message })
+}
+
 fchat.onOpen(ticket => {
     console.log(`Websocket connection opened. Identifying with ticket: ${ticket}`);
 });
@@ -16,17 +31,55 @@ fchat.on("ERR", event => {
     console.log('ERR', event)
 })
 
-fchat.on("JCH", event => {
-    console.log('Joined channel', event.title)
-})
-
 fchat.on("CON", () => {
     // [session=Truth or Dare, Pie Corner]adh-3d665c7ad3a74fcd1b4b[/session]
     // [session=Bot test - ignore me]adh-34e712245998e51b61e3[/session]
 
-    // fchat.send("JCH", { channel: 'adh-3d665c7ad3a74fcd1b4b' });
-    fchat.send("JCH", { channel: 'adh-34e712245998e51b61e3' });
+    fchat.send("JCH", { channel: 'adh-3d665c7ad3a74fcd1b4b' });
+    // fchat.send("JCH", { channel: 'adh-34e712245998e51b61e3' });
     // fchat.send("JCH", { channel: 'development' });
+})
+
+fchat.on("JCH", ({channel, character, title}) => {
+    if (character.identity === 'Game Bot') {
+        console.log('Joined channel', title)
+        playerTracker[channel] = []
+    }
+})
+
+const joinCommands = ['!yesspin', '!optin', '!join']
+const leaveCommands = ['!nospin', '!optout', '!leave']
+const bottleSpinCommands = ['!spin', '!bottle']
+
+fchat.on("MSG", ({character, message, channel}) => {
+    const xmessage = message.toLowerCase()
+    if (xmessage === 'Hey Game Bot') {
+        sendMSG(channel, `Hey yourself, ${wrapInUserTags(character)}!`)
+    } else if (joinCommands.includes(xmessage)) {
+        if (!playerTracker[channel].includes(character)) {
+            playerTracker[channel].push(character)
+            sendMSG(channel, `${wrapInUserTags(character)} has joined the game! ${currentPlayersList(channel)}`)
+        } else {
+            sendMSG(channel, `You're already in the game, ${wrapInUserTags(character)}!`)
+        }
+    } else if (leaveCommands.includes(xmessage)) {
+        if (playerTracker[channel].includes(character)) {
+            playerTracker[channel] = playerTracker[channel].filter(c => c !== character)
+            sendMSG(channel, `${wrapInUserTags(character)} has left the game. ${currentPlayersList(channel)}`)
+        } else {
+            sendMSG(channel, `You're already not in the game, ${wrapInUserTags(character)}.`)
+        }
+    } else if (bottleSpinCommands.includes(xmessage)) {
+        if (!playerTracker[channel].includes(character)) {
+            sendMSG(channel, `You can't spin if you aren't playing, ${wrapInUserTags(character)}! Join the game first!`)
+        } else if (playerTracker[channel].length < 3) {
+            sendMSG(channel, `There aren't enough players in the game. 3 players are required in order to spin. ${currentPlayersList(channel)}`)
+        } else {
+            const eligiblePlayers = playerTracker[channel].filter(c => c !== character)
+            const chosenPlayer = playerTracker[channel][Math.floor(Math.random() * (eligiblePlayers.length))-1]
+            sendMSG(channel, `${wrapInUserTags(character)} spins the bottle! It points to... ${wrapInUserTags(chosenPlayer)}!`)
+        }
+    }
 })
 
 fchat.connect(connectionInfo.account, connectionInfo.password, connectionInfo.character);
