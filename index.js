f3e7d7ffbcea9de4a1f623875902e2d823e06efa
BNicoleSchmidt <the.ringwyrm@gmail.com>
2021-07-21 17:11:52 -0500

    Send players list privately on request, message queue for 1 msg/sec

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fchat = new Fchat(config);
 
 const playerTracker = {}
 const channelTracker = {}
+const messageQueue = []
 let isAlive = false
 
 function wrapInUserTags(character) {
@@ -20,20 +21,29 @@ function formatCommands(commands) {
     return commands.map(boldText).join(', ')
 }
 
-function currentPlayersList(channel) {
+function currentPlayersCount(channel) {
     const players = playerTracker[channel]
     const plural = players.length !== 1
-    return players.length ? `There ${plural ? 'are' : 'is'} currently ${players.length} player${plural ? 's' : ''}:\n${players.map(wrapInUserTags).join('\n')}` : 'No one is currently playing.'
+    return players.length ? `There ${plural ? 'are' : 'is'} currently ${players.length} player${plural ? 's' : ''}.` : 'No one is currently playing.'
+}
+
+function currentPlayersList(channel) {
+    const players = playerTracker[channel]
+    return `Current players in ${channelTracker[channel].title}: \n${players.map(wrapInUserTags).join('\n')}`
 }
 
 function sendMSG(channel, message) {
-    fchat.send('MSG', { channel, message })
+    messageQueue.push({ code: 'MSG', payload: { channel, message } })
+}
+
+function sendPRI(recipient, message) {
+    messageQueue.push({ code: 'PRI', payload: { recipient, message } })
 }
 
 function addPlayer(character, channel) {
     if (!playerTracker[channel].includes(character)) {
         playerTracker[channel].push(character)
-        sendMSG(channel, `${wrapInUserTags(character)} has joined the game! ${currentPlayersList(channel)}`)
+        sendMSG(channel, `${wrapInUserTags(character)} has joined the game! ${currentPlayersCount(channel)}`)
     } else {
         sendMSG(channel, `You're already in the game, ${wrapInUserTags(character)}!`)
     }
@@ -42,7 +52,7 @@ function addPlayer(character, channel) {
 function removePlayer(character, channel, disconnect = false) {
     if (playerTracker[channel].includes(character)) {
         playerTracker[channel] = playerTracker[channel].filter(c => c !== character)
-        sendMSG(channel, `${wrapInUserTags(character)} has left the game. ${currentPlayersList(channel)}`)
+        sendMSG(channel, `${wrapInUserTags(character)} has left the game. ${currentPlayersCount(channel)}`)
     } else if (!disconnect) {
         sendMSG(channel, `You're already not in the game, ${wrapInUserTags(character)}.`)
     }
@@ -54,7 +64,7 @@ function spin(character, channel) {
     if (!playerTracker[channel].includes(character)) {
         sendMSG(channel, `You can't spin if you aren't playing, ${wrapInUserTags(character)}! Join the game first!`)
     } else if (playerTracker[channel].length < requiredPlayers) {
-        sendMSG(channel, `There aren't enough players in the game. ${requiredPlayers} players are required in order to spin. ${currentPlayersList(channel)}`)
+        sendMSG(channel, `There aren't enough players in the game. ${requiredPlayers} players are required in order to spin. ${currentPlayersCount(channel)}`)
     } else {
         const eligiblePlayers = playerTracker[channel].filter(c => c !== character && (!channelInfo.preventSpinback || c !== channelInfo.lastSpinner))
         const chosenPlayer = eligiblePlayers[Math.floor(Math.random() * (eligiblePlayers.length))]
@@ -84,19 +94,21 @@ fchat.on("CON", () => {
     // fchat.send("JCH", { channel: 'development' });
 })
 
-fchat.on("JCH", ({channel, character, title}) => {
+fchat.on("JCH", ({ channel, character, title }) => {
     if (character.identity === 'Game Bot') {
         console.log('Joined channel', title)
-        playerTracker[channel] = []
-        channelTracker[channel] = {preventSpinback: channel.toLowerCase() === 'adh-3d665c7ad3a74fcd1b4b'}
+        if (!Object.keys(channelTracker).includes(channel)) {
+            playerTracker[channel] = []
+            channelTracker[channel] = { preventSpinback: true, title }
+        }
     }
 })
 
-fchat.on("LCH", ({channel, character}) => {
+fchat.on("LCH", ({ channel, character }) => {
     removePlayer(character, channel, true)
 })
 
-fchat.on("FLN", ({character}) => {
+fchat.on("FLN", ({ character }) => {
     const allChannels = Object.keys(playerTracker)
     for (const channel of allChannels) {
         removePlayer(character, channel, true)
@@ -110,7 +122,7 @@ const bottleSpinCommands = ['!spin', '!bottle']
 const spinbackCommands = ['!spinback', '!togglespinback']
 const helpCommands = ['!help', '!commands', '!info']
 
-fchat.on("MSG", ({character, message, channel}) => {
+fchat.on("MSG", ({ character, message, channel }) => {
     const xmessage = message.toLowerCase().trim()
     if (xmessage === 'hey game bot') {
         sendMSG(channel, `Hey yourself, ${wrapInUserTags(character)}!`)
@@ -121,7 +133,10 @@ fchat.on("MSG", ({character, message, channel}) => {
     } else if (bottleSpinCommands.includes(xmessage)) {
         spin(character, channel)
     } else if (statusCommands.includes(xmessage)) {
-        sendMSG(channel, currentPlayersList(channel))
+        sendMSG(channel, currentPlayersCount(channel))
+        if (playerTracker[channel].length) {
+            sendPRI(character, currentPlayersList(channel))
+        }
     } else if (spinbackCommands.includes(xmessage)) {
         newSetting = !channelTracker[channel].preventSpinback
         channelTracker[channel].preventSpinback = newSetting
@@ -160,5 +175,12 @@ setInterval(function ping() {
     isAlive = false;
     fchat.socket.ping(() => { });
 }, 30000);
+
+setInterval(function send() {
+    const msg = messageQueue.shift(1)
+    if (msg) {
+        fchat.send(msg.code, msg.payload)
+    }
+}, 1001)
 
 connect()
