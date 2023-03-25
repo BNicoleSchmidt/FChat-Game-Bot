aab28ee29924f4982e728a36465a9d4ae6354a1b
BNicoleSchmidt <the.ringwyrm@gmail.com>
2023-03-25 08:11:40 -0500

    Update Pokemon list

--- fetch-pokemon.js
+++ fetch-pokemon.js
@@ -15,7 +15,7 @@ function recursiveFetchPokemon(id) {
             const forms = ['normal', ...json.varieties.filter(v => !v.is_default).map(v => getForm(v.pokemon.name, json.name))]
             pokemon.push({ id, name: json.name, genderRate: json.gender_rate, forms })
             console.log('Fetched', id)
-            if (id < 898) {
+            if (id < 1010) {
                 recursiveFetchPokemon(id + 1)
             } else {
                 fs.writeFile('pokemon.json', JSON.stringify(_.sortBy(pokemon, 'id'), 0, 4), err => { })
